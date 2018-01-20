27502a42f934f355b16a8ff6241c5fba1467f9ac
Andrew Nicolalde <Andrew.Nicolalde.2016@live.rhul.ac.uk>
2018-01-20 15:09:14 +0000

    Add dynamic gluten-free status icon

--- server/src/static/js/menuretrieve-waiter.js
+++ server/src/static/js/menuretrieve-waiter.js
@@ -11,9 +11,12 @@ $(document).ready(function() {
 
     // Add items to menu list
     for(i = 0; i < response.length; i++) {
-      $("#menu-list").append("<li class='list-group-item'><span class='waiter-ui-span-bold'>"
+      $("#menu-list").append("<li class='list-group-item' id= \"menu-item-"+ i +"\"><span class='waiter-ui-span-bold'>"
           + response[i].name + ":</span> " + response[i].price + "</li>");
       // TODO: Show dietary information
+      if(response[i].is_gluten_free){
+        $("#menu-item-" + i).append("<img src='../images/gluten_free.png' alt='Gluten Free'>");
+      }
     }
   });
 });
