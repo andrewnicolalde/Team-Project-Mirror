3637c21d14b827f7f9720dcb2809f5ecd29326f3
Andrew Nicolalde <Andrew.Nicolalde.2016@live.rhul.ac.uk>
2018-01-20 16:22:23 +0000

    Make vegetarian and vegan icons dynamic
    Also switch gluten-free icon over to svg

--- server/src/static/js/menuretrieve-waiter.js
+++ server/src/static/js/menuretrieve-waiter.js
@@ -13,9 +13,15 @@ $(document).ready(function() {
     for(i = 0; i < response.length; i++) {
       $("#menu-list").append("<li class='list-group-item' id= \"menu-item-"+ i +"\"><span class='waiter-ui-span-bold'>"
           + response[i].name + ":</span> " + response[i].price + "</li>");
-      // TODO: Show dietary information
-      if(response[i].is_gluten_free){
-        $("#menu-item-" + i).append("<img src='../images/gluten_free.png' alt='Gluten Free'>");
+      // Show dietary information
+      if(response[i].is_gluten_free){ // Gluten Free
+        $("#menu-item-" + i).append("<img src='../images/gluten-free.svg' alt='Gluten Free'>");
+      }
+      if(response[i].is_vegetarian){ // Vegetarian
+        $("#menu-item-" + i).append("<img src='../images/vegetarian-mark.svg' alt='Vegetarian'>");
+      }
+      if(response[i].is_vegan){
+        $("#menu-item-" + i).append("<img src='../images/vegan-mark.svg' alt='Vegan'>");
       }
     }
   });
