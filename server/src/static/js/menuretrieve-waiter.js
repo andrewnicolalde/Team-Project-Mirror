35eed7b7b87ff12155d2e949bd71f4e35c948dcc
Andrew Nicolalde <Andrew.Nicolalde.2016@live.rhul.ac.uk>
2018-01-20 14:24:56 +0000

    Add dynamic menu loading
    Also add a couple of buttons for confirm and cancel order, they don't do
    anything yet.

---
+++ server/src/static/js/menuretrieve-waiter.js
@@ -0,0 +1,19 @@
+/*
+This script should be used only for the waiter's UI. This is because this script
+only displays the name of each menu item & its price. Obviously a customer would
+want to see more than that.
+ */
+$(document).ready(function() {
+  // Send get request to server for menu JSON
+  $.get("/api/menu", function(data){
+    // Parse JSON
+    var response = JSON.parse(data);
+
+    // Add items to menu list
+    for(i = 0; i < response.length; i++) {
+      $("#menu-list").append("<li class='list-group-item'><span class='waiter-ui-span-bold'>"
+          + response[i].name + ":</span> " + response[i].price + "</li>");
+      // TODO: Show dietary information
+    }
+  });
+});
