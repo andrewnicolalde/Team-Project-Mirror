6f8aaae3470d579012ba242f107ecc6fd4f48459
Marcus <marcus.messer.2016@live.rhul.ac.uk>
2018-01-20 17:05:30 +0000

    Created an OrderStatus enum for use in the order table.

---
+++ server/src/database/OrderStatus.java
@@ -0,0 +1,13 @@
+package database;
+
+/**
+ * This enum is used to represent the status of the order.
+ *
+ * @author Marcus Messer
+ */
+public enum OrderStatus {
+  CANCELED,
+  COOKED,
+  READY_TO_COLLECT,
+  CONFIRMED
+}
