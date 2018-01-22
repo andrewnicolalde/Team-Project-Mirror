bba4be7b6fe55d1b36ce6c668e10742882af36a9
rogermilroy <Roger.Milroy.2016@live.rhul.ac.uk>
2018-01-22 21:27:02 +0000

    Added endpoint for Kitchen. Early thoughts on function to update kitchen view of Orders.

---
+++ server/src/endpoints/kitchen/Order.java
@@ -0,0 +1,9 @@
+package endpoints.kitchen;
+
+public class Order {
+
+  public static String getOrder() {
+    return "\"{\"orderNo\": \"1\", \"item\": \"Burger\", \"requirements\":\"None\"}\"";
+  }
+
+}
