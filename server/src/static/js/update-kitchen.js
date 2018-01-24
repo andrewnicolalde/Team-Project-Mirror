99ada103d3bd71f7f4736eda4d2b9706e5cdbec2
rogermilroy <Roger.Milroy.2016@live.rhul.ac.uk>
2018-01-24 15:17:09 +0000

    Added Kitchen test file and a test to validate using Qunit.

---
+++ server/src/static/js/update-kitchen.js
@@ -0,0 +1,24 @@
+/**
+ * Preliminary method to update the page dynamically with new orders.
+ * @param data the data response to an ajax query.
+ */
+function updatePage(data) {
+
+  var response = JSON.parse(data);
+
+  for (i = 0; i < response.length; i++) {
+    // first item or item on same order (assuming grouped by order)
+    if (i === 0 || response[i].orderNo === response[i-1].orderNo) { // this might be a bad check.
+      // set the order number to be a class of the column to separate them.
+      $(".row").append("<div class=\"col "+ response[i].orderNo +"\">" +
+          "<div class='card text-center'" +
+          "<div class='card-header'>Order No: " + response[i].orderNo + "</div>" +
+          "<div class='card-body'>Item: " + response[i].item +
+          "<br />Requirements: " + response[i].requirements +
+          "</div>" +
+          "</div>" +
+          "</div>");
+    }
+  }
+}
+
