10c966408790b3c48862cf423bef4fe8fc979ca9
rogermilroy <rogermilroy@icloud.com>
2018-01-25 22:35:17 +0000

    Change location of Main class in pom and reenable port finding in Main

--- server/src/Main.java
+++ server/src/Main.java
@@ -33,11 +33,11 @@ public class Main {
   public static void main(String[] args) {
     staticFileLocation("static"); // Lets spark know where the static files are
 
-    /*
+    
     // Uncomment this if you are pushing to Heroku
     int port = Integer.parseInt(System.getenv("PORT"));
     port(port);
-    */
+
 
     // Setup the database connector
     connector = Connector.getInstance();
