c9f582542f18719495f899e69672010a34869993
robin chabouk <robin.chabouk.2016@live.rhul.ac.uk>
2018-01-25 12:14:37 +0000

    add jquery for basket and refactor retrieveorder-customer.js
    js doesn't currently run due to endpoint not being setup. if sent to endpoint now, server crashes. When new endpoint is setup, path will need to be updated in js file.

--- server/src/static/js/retrieveorder-customer.js
+++ server/src/static/js/retrieveorder-customer.js
@@ -2,24 +2,17 @@
  *This script retrieves the current order for a given
  *table so that it can be displayed in the basket page.
  */
-
-
-
-//function loadOrder(tableNumber){
-    document.write("An order");
-    var tableNumberToSend = JSON.stringify({tableNumber: tableNumber});
-    post("path", tableNumberToSend, function(data){
+$(document).ready(function () {
+    // TODO: Update path once real endpoint is set up
+    post("/api/auth/getOrder", function (data) {
         //parse JSON
         var response = JSON.parse(data);
-        var superelement = document.getElementById("current-order");
-        //add items to order list
-        while(superelement.firstChild){
-            superelement.removeChild(superelement.firstChild);
-        }
         for (i = 0; i < response.length; i++) {
-            $("#current-order").append("<li class = 'list-group-item list-group-item-action' id= \"order-item-"+i+"\"><span class='custmomer-ui-span-bold'>"
-                +response[i].name+": </span>"+response[i].price+"</li>");
-
+            $("#table-body").append("<tr>" +
+                "<th scope=\"row\">" + (i + 1) + "</th>" +
+                "<td>" + response[i].name + "</td>" +
+                "<td>" + response[i].price + "</td>" +
+                "</tr>");
         }
+    });
 });
-//}
