4d6696d5e6e073f15bd38b1d0740ebf0fb3c8928
rogermilroy <rogermilroy@icloud.com>
2018-01-25 22:30:18 +0000

    Removed unused imports to enable maven build

--- server/src/endpoints/order/Orders.java
+++ server/src/endpoints/order/Orders.java
@@ -1,31 +1,22 @@
 package endpoints.order;
 
 import com.google.gson.Gson;
-import com.sun.org.apache.xpath.internal.operations.Or;
 import database.Connector;
 import database.tables.FoodOrder;
 import database.tables.MenuItem;
 import database.tables.OrderMenuItem;
-import database.tables.Staff;
 import database.tables.StaffSession;
-import database.tables.TableSession;
-import javafx.scene.control.ListCell;
-import org.hibernate.criterion.Order;
+import java.util.List;
 import spark.Request;
 import spark.Response;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.List;
-
 public class Orders {
+
   private static final Gson GSON = new Gson();
 
   /**
-   * Returns an order as JSON.
-   * JSON input:
-   *     tableNumber: an integer representing the table number
+   * Returns an order as JSON. JSON input: tableNumber: an integer representing the table number
+   *
    * @param request A HTTP request object.
    * @param response A HTTP response object.
    * @return A string containing JSON which holds the current order.
@@ -38,10 +29,10 @@ public class Orders {
 
   /**
    * Returns the order menu items from the database in JSON format.
+   *
    * @param tableNumber The number of the table.
    * @param staffId The employee number for the staff member.
    * @return The menu items for the table in a JSON format.
-   *
    * @author Marcus Messer
    */
   public static String getOrderMenuItems(Long tableNumber, Long staffId) {
@@ -54,7 +45,6 @@ public class Orders {
         + "orderMenuItem.foodOrder.transaction.restaurantTableStaff.staff.employeeNumber = "
         + staffId, OrderMenuItem.class);
 
-
     connector.closeConnection();
 
     CustomerOrderData[] customerOrderData = new CustomerOrderData[orderMenuItems.size()];
@@ -65,11 +55,10 @@ public class Orders {
   }
 
   /**
-   * Adds an orderMenuItem to an order.
-   * JSON input:
-   *     tableNumber: An integer representing the table number
-   *     menuItemId: An integer representing the id of the MenuItem to add to the order.
-   *     description: A string representing a description/extra details for the order.
+   * Adds an orderMenuItem to an order. JSON input: tableNumber: An integer representing the table
+   * number menuItemId: An integer representing the id of the MenuItem to add to the order.
+   * description: A string representing a description/extra details for the order.
+   *
    * @param request A HTTP request object.
    * @param response A HTTP response object.
    * @return A string saying either "success" or "failed"
@@ -105,25 +94,24 @@ public class Orders {
   }
 
   /**
-   * Changes the order status
-   * JSON input:
-   *     tableNumber: An integer representing the table number.
-   *     newOrderStatus: A string representing the new order status. This can be CANCELLED,
-   ORDERING, READY_TO_CONFIRM, COOKING, READY_TO_DELIVER or DELIVERED.
+   * Changes the order status JSON input: tableNumber: An integer representing the table number.
+   * newOrderStatus: A string representing the new order status. This can be CANCELLED, ORDERING,
+   * READY_TO_CONFIRM, COOKING, READY_TO_DELIVER or DELIVERED.
+   *
    * @param request A HTTP request object.
    * @param response A HTTP response object.
    * @return A string saying either "success" or "failed"
    */
   public static String changeOrderStatus(Request request, Response response) {
-    ChangeOrderStatusParameters cos = GSON.fromJson(request.body(), ChangeOrderStatusParameters.class);
+    ChangeOrderStatusParameters cos = GSON
+        .fromJson(request.body(), ChangeOrderStatusParameters.class);
     return "success";
   }
 
   /**
-   * Removes an item from an order
-   * JSON input:
-   *     tableNumber: An integer representing the table number.
-   *     menuItemId: An integer representing the id of the MenuItem to remove from the order.
+   * Removes an item from an order JSON input: tableNumber: An integer representing the table
+   * number. menuItemId: An integer representing the id of the MenuItem to remove from the order.
+   *
    * @param request A HTTP request object.
    * @param response A HTTP response object.
    * @return A string saying either "success" or "failed"
