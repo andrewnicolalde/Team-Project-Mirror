f7730b5feaeb744f39d43eb10df89474378669ba
Marcus Messer <marcus.messer.2016@live.rhul.ac.uk>
2018-01-31 22:26:32 +0000

    Created persistence changes for use with multiple databases.

--- server/tests/database/tables/RestaurantTableTest.java
+++ server/tests/database/tables/RestaurantTableTest.java
@@ -18,7 +18,7 @@ public class RestaurantTableTest {
   @Before
   public void setUp() {
     //Create link to the database
-    entityManagerFactory = Persistence.createEntityManagerFactory("server.database");
+    entityManagerFactory = Persistence.createEntityManagerFactory("server.database.test");
   }
 
   @After
