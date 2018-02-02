ccc044e09b4947baf7767474e1d47dccef60cc63
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2018-02-02 12:13:43 +0000

    Add add menu items to order functionality
    Still need to make sure current order is being refreshed after every time a new menu item is added to the order, also need to find out why the server is 500 server erroring when I make these requests. Toby suspects the bug is due to something in authentication.

--- server/src/static/js/add-to-order.js
+++ server/src/static/js/add-to-order.js
@@ -1,16 +1,37 @@
-function addToOrder(tableNumber, menuItemId, description){
+/**
+ * This function is responsible for adding menu items to the current order
+ * of the currently active table.
+ * @param menuItemId
+ * @param description
+ */
+// TODO: Add a description attribute here so that additional preparation info can be provided.
+function addToOrder(menuItemId){
+  console.log("asdfasdfasdfasdfasdfasdf");
+
+  // Find active table
+  var allTables = document.getElementById("tables-list").children;
+  var activeTable;
+  for(i = 0; i < allTables.length; i++){
+    if(allTables[i].classList.contains("active")){
+      activeTable = allTables[i];
+    }
+  }
+
+  // TODO: Remove this and add an actual description box in the UI
+  var description = "This is a test descripion"
+
   // Create name-value pairs for HTTP post request, see
   // https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
   var nameValuePairs = JSON.stringify({
-    tableNumber: tableNumber,
+    tableNumber: activeTable.getAttribute('data-tablenum'),
     menuItemID: menuItemId,
     description: description
   });
+
   // Handle possible responses
-  post("/api/auth/addToOrder", nameValuePairs, function(status){
-    if(status === "success"){
-      loadOrder();
-    } else {
+  post("/api/authStaff/addToOrder", nameValuePairs, function(status){
+    loadOrder();
+    if(status === ""){
       // Refresh current order table to show new change
       console.log("Add item to order failed");
       console.log(status);
