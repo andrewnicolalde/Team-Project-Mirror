fc1b4f3978b88d53d4a6faea3f57be4f6f9bf7f6
Jonathan Lewin <zdac014@live.rhul.ac.uk>
2018-02-04 16:26:58 +0000

    Modified the jquery code

--- Server/src/static/js/getmenu-customer.js
+++ Server/src/static/js/getmenu-customer.js
@@ -4,6 +4,21 @@ $(document).ready(function() {
     // Parse JSON
     var response = JSON.parse(data);
 
+    function drawTable() {
+        for (var i = 0; i < response.length; i++) {
+            drawRow(response[i]);
+        }
+    }
+
+    function drawRow(rowData) {
+        var row = $("<tr />")
+        $("#menu-table").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+        row.append($("<td>" + rowData.name + "</td>"));
+        row.append($("<td>" + rowData.price + "</td>"));
+    }
+
+
+
     for(i = 0; i < response.length; i++) {
         $("#menu-table").append("<tr>" +
             "<th scope=\"row\">" + (i + 1) + "</th>" +
