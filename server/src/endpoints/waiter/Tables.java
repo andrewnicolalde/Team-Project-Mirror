5588624afdd8e06ae9e7fed8af131deecdb273f6
Marcus Messer <marcus.messer.2016@live.rhul.ac.uk>
2018-02-06 11:24:42 +0000

    Integrated the table endpoint into the database

--- server/src/endpoints/waiter/Tables.java
+++ server/src/endpoints/waiter/Tables.java
@@ -3,7 +3,9 @@ package endpoints.waiter;
 import com.google.gson.Gson;
 import database.DatabaseManager;
 import database.tables.RestaurantTableStaff;
+import database.tables.StaffSession;
 import javax.persistence.EntityManager;
+import javax.xml.crypto.Data;
 import spark.Request;
 import spark.Response;
 
@@ -22,12 +24,14 @@ public class Tables {
   public static String getTables(Request request, Response response) {
     // TODO: Get tables from the database
 
-    return "[{\"number\":1,\"status\":\"Ready to order\",\"franchise\":3},{\"number\":2,\"status\":\"Eating\",\"franchise\":3}]";
+    EntityManager entityManager = DatabaseManager.getInstance().getEntityManager();
+    StaffSession staffSession = entityManager.find(StaffSession.class, request.session().attribute(
+        "StaffSessionKey"));
+    return getTableData(staffSession.getStaff().getEmployeeNumber());
   }
 
   public static String getTableData(Long staffId) {
     EntityManager entityManager = DatabaseManager.getInstance().getEntityManager();
-    entityManager.getTransaction().begin();
     List<RestaurantTableStaff> restaurantTableStaffs = entityManager.createQuery("from " +
         "RestaurantTableStaff tableStaff where tableStaff.staff.employeeNumber = " + staffId,
         RestaurantTableStaff.class).getResultList();
@@ -36,8 +40,6 @@ public class Tables {
     for (int i = 0; i < tableData.length; i++) {
       tableData[i] = new TableData(restaurantTableStaffs.get(i));
     }
-
-    entityManager.getTransaction().commit();
 
     return GSON.toJson(tableData);
   }
