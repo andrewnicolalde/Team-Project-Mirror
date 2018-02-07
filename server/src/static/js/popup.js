e688c93dcec640a1b1f03345dec5a944c214ec3a
Jonathan Lewin <zdac014@live.rhul.ac.uk>
2018-02-07 00:04:03 +0000

    Continuing to add more jquery, plus the start of drop down menus in the customer ui

--- server/src/static/js/popup.js
+++ server/src/static/js/popup.js
@@ -2,7 +2,32 @@ $(document).ready(function(){
     $('.myBtn').click(function(){
         $("#myModal").modal();
     });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 });
+
+
+
+
+
+
 
 
 $(document).ready(function(){
