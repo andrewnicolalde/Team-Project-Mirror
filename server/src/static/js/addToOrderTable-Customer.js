c286237dce192ab3957f5b514e93d496008f4546
rogermilroy <rogermilroy@icloud.com>
2018-02-08 17:41:15 +0000

    load orders after adding them to order.

--- server/src/static/js/addToOrderTable-Customer.js
+++ server/src/static/js/addToOrderTable-Customer.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  loadOrder();
   /*$(".add-row").click(function () {
     var name = $(".name").val();
     var price = $(".price").val();
@@ -37,6 +38,43 @@ function addToOrder(menuItemId) {
       // Refresh current order table to show new change
       console.log("Add item to order failed");
       console.log(status);
+    }
+  });
+}
+
+function loadOrder() {
+  var tableNumberToSend = JSON.stringify({tableNumber: 1});
+  post("/api/authStaff/getOrder", tableNumberToSend, function (data) {
+
+    // Parse JSON
+    var response = JSON.parse(data);
+
+    // Remove any existing elements in the current order list
+    var currentOrderElement = document.getElementById("current-order");
+    while (currentOrderElement.firstChild) {
+      currentOrderElement.removeChild(currentOrderElement.firstChild);
+    }
+
+    // Add each list item
+    for (i = 0; i < response.length; i++) {
+      $("#current-order").append("<li class='list-group-item list-group-item-action'"
+          + "id= \"order-item-" + i + "\">"
+          + "<span class='waiter-ui-span-bold'>"
+          + response[i].name + ": </span> "
+          + response[i].price + "</li>");
+      // Show dietary information
+      if (response[i].is_gluten_free) { // Gluten Free
+        $("#order-item-" + i).append(" <img src="
+            + "'../../images/gluten-free.svg'alt='Gluten Free'>");
+      }
+      if (response[i].is_vegetarian) { // Vegetarian
+        $("#order-item-" + i).append(" <img src="
+            + "'../../images/vegetarian-mark.svg'alt='Vegetarian'>");
+      }
+      if (response[i].is_vegan) {
+        $("#order-item-" + i).append(" <img src="
+            + "'../../images/vegan-mark.svg'alt='Vegan'>");
+      }
     }
   });
 }
