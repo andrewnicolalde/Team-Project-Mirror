49231125797a1bf0c1390708bf1910a835470e75
rogermilroy <rogermilroy@icloud.com>
2018-02-08 18:20:16 +0000

    Fix merge break issues rename and move js functions

--- server/src/static/js/addToOrderTable-Customer.js
+++ server/src/static/js/addToOrderTable-Customer.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   loadOrder();
+  loadMenu();
   /*$(".add-row").click(function () {
     var name = $(".name").val();
     var price = $(".price").val();
@@ -22,11 +23,42 @@ $(document).ready(function () {
   });*/
 });
 
+function loadMenu() {
+  // Send get request to server for menu JSON
+  get("/api/authStaff/menu", function (data) {
+    // Parse JSON
+    var response = JSON.parse(data);
+
+    // Add items to menu list
+    for (i = 0; i < response.length; i++) {
+      $("#menu-list").append("<li class='list-group-item list-group-item-action'"
+          + "id= \"menu-item-" + i + "\""
+          + "data-menuItemNum='" + response[i].id + "'"
+          + "onclick='addToOrder(this.getAttribute(\"data-menuItemNum\"))'>"
+          + "<span class='waiter-ui-span-bold'>"
+          + response[i].name + ": </span> " + response[i].price + "</li>");
+      // Show dietary information
+      if (response[i].is_gluten_free) { // Gluten Free
+        $("#menu-item-" + i).append(
+            " <img src='../images/gluten-free.svg' alt='Gluten Free'>");
+      }
+      if (response[i].is_vegetarian) { // Vegetarian
+        $("#menu-item-" + i).append(
+            " <img src='../images/vegetarian-mark.svg' alt='Vegetarian'>");
+      }
+      if (response[i].is_vegan) {
+        $("#menu-item-" + i).append(
+            " <img src='../images/vegan-mark.svg' alt='Vegan'>");
+      }
+    }
+  });
+}
+
 function addToOrder(menuItemId) {
   // Create name-value pairs for HTTP post request, see
   // https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
   var nameValuePairs = JSON.stringify({
-    tableNumber: 1,
+    orderNumber: 2,
     menuItemId: menuItemId,
     instructions: "none"
   });
@@ -43,8 +75,8 @@ function addToOrder(menuItemId) {
 }
 
 function loadOrder() {
-  var tableNumberToSend = JSON.stringify({tableNumber: 1});
-  post("/api/authStaff/getOrder", tableNumberToSend, function (data) {
+  var orderNumberToSend = JSON.stringify({orderNumber: 2});
+  post("/api/authStaff/getOrder", orderNumberToSend, function (data) {
 
     // Parse JSON
     var response = JSON.parse(data);
@@ -65,15 +97,15 @@ function loadOrder() {
       // Show dietary information
       if (response[i].is_gluten_free) { // Gluten Free
         $("#order-item-" + i).append(" <img src="
-            + "'../../images/gluten-free.svg'alt='Gluten Free'>");
+            + "'../images/gluten-free.svg'alt='Gluten Free'>");
       }
       if (response[i].is_vegetarian) { // Vegetarian
         $("#order-item-" + i).append(" <img src="
-            + "'../../images/vegetarian-mark.svg'alt='Vegetarian'>");
+            + "'../images/vegetarian-mark.svg'alt='Vegetarian'>");
       }
       if (response[i].is_vegan) {
         $("#order-item-" + i).append(" <img src="
-            + "'../../images/vegan-mark.svg'alt='Vegan'>");
+            + "'../images/vegan-mark.svg'alt='Vegan'>");
       }
     }
   });
