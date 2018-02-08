9764387ca5165c81bfa03447717e496c17a1dea8
rogermilroy <rogermilroy@icloud.com>
2018-02-08 17:18:38 +0000

    Completely changed customer menu page to use elements from waiter UI.

--- server/src/static/js/addToOrderTable-Customer.js
+++ server/src/static/js/addToOrderTable-Customer.js
@@ -1,21 +1,42 @@
-$(document).ready(function(){
-        $(".add-row").click(function(){
-            var name = $(".name").val();
-            var price = $(".price").val();
-            var markup = "<tr><td></td><td>" + name + "</td><td>" + price + "</td></tr>";
-            $("table.cart tbody").append(markup);
-        });
+$(document).ready(function () {
+  /*$(".add-row").click(function () {
+    var name = $(".name").val();
+    var price = $(".price").val();
+    var markup = "<tr><td></td><td>" + name + "</td><td>" + price
+        + "</td></tr>";
+    $("table.cart tbody").append(markup);
+  });*/
 
-    /*$("td").click(function () {
-          $(".name").clone().appendTo(".cart");
-        });
+  /*$("td").click(function () {
+    $(".name").clone().appendTo(".cart");
+  });*/
 
-        // Find and remove selected table rows
-        $(".add-order").click(function(){
-            $("table tbody").find('input[name="record"]').each(function(){
-            	if($(this).is(":checked")){
-                    $(".name").clone().appendTo(".cart");
-                }
-            });
-        });*/
+  // Find and remove selected table rows
+  /*$(".add-order").click(function () {
+    $("table tbody").find('input[name="record"]').each(function () {
+      if ($(this).is(":checked")) {
+        $(".name").clone().appendTo(".cart");
+      }
     });
+  });*/
+});
+
+function addToOrder(menuItemId) {
+  // Create name-value pairs for HTTP post request, see
+  // https://en.wikipedia.org/wiki/POST_(HTTP)#Use_for_submitting_web_forms
+  var nameValuePairs = JSON.stringify({
+    tableNumber: 1,
+    menuItemId: menuItemId,
+    instructions: "none"
+  });
+
+  // Handle possible responses
+  post("/api/authStaff/addToOrder", nameValuePairs, function (status) {
+    loadOrder();
+    if (status === "") {
+      // Refresh current order table to show new change
+      console.log("Add item to order failed");
+      console.log(status);
+    }
+  });
+}
