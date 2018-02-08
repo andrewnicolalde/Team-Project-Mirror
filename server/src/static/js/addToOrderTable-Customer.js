ccc1d0642562b8ce55cd3b3cfd2774d97d1b6719
JohanRehman <zdac008@live.rhul.ac.uk>
2018-02-08 12:11:55 +0000

    Add JS into separate file.

--- server/src/static/js/addToOrderTable-Customer.js
+++ server/src/static/js/addToOrderTable-Customer.js
@@ -1,7 +1,20 @@
- $(document).ready(function(){
-        $(".add-row").click(function()) {
-            var name = $("#item").val();
-            var email = $("#price").val();
-            var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
-            $("table tbody").append(markup);
+$(document).ready(function(){
+        $(".add-row").click(function(){
+            var name = $(".name").val();
+            var price = $(".price").val();
+            var markup = "<tr><td></td><td>" + name + "</td><td>" + price + "</td></tr>";
+            $("table.cart tbody").append(markup);
         });
+    $("td").click(function () {
+          $(".name").clone().appendTo(".cart");
+        });
+
+        // Find and add selected table rows to cart table
+        $(".add-order").click(function(){
+            $("table tbody").find('input[name="record"]').each(function(){
+            	if($(this).is(":checked")){
+                    $(".name").clone().appendTo(".cart");
+                }
+            });
+        });
+    });
