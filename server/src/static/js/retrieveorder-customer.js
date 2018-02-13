02da708f6a4fdafc8a05381d4173f4900d827864
robin chabouk <robin.chabouk.2016@live.rhul.ac.uk>
2018-02-13 12:17:28 +0000

    removed the quantity column from the basket table and modified script for retrieving menu items.

--- server/src/static/js/retrieveorder-customer.js
+++ server/src/static/js/retrieveorder-customer.js
@@ -4,18 +4,19 @@
  */
 $(document).ready(function () {
     // TODO: Update path once real endpoint is set up
-    post("/api/authStaff/getOrder", function (data) {
+    var orderNumberToSend = JSON.stringify({orderNumber: 2});
+    post("/api/authStaff/getOrder", orderNumberToSend, function (data) {
         //parse JSON
         var response = JSON.parse(data);
         for (i = 0; i < response.length; i++) {
             $("#table-body").append("<tr data-toggle=\"collapse\" data-target=\"#row\"" + (i + 1) + " class=\"clickable\">" +
-                "<th scope=\"row\">" + (i + 1) + "</th>" +
+                "<th scope=\"row\">" + (i + 4) + "</th>" +
                 "<td>" + response[i].name + "</td>" +
                 "<td>" + response[i].price + "</td>" +
                 "</tr>" +
                 "<tr>" +
-                "<td colspan=4>" +
-                "<div id=\"row\"" + (i + 1) + " class=\"collapse\">" + response[i].details + "</div>" +
+                "<td colspan=\"3\">" +
+                "<div id=\"row\"" + (i + 4) + " class=\"collapse\">" + response[i].instructions+ "</div>" +
                 "</td>" +
                 "</tr>"
             );
