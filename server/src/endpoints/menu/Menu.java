66bde39306600c27d8ac2ce030334f9bae5c24bd
Marcus Messer <marcus.messer.2016@live.rhul.ac.uk>
2018-02-13 12:17:40 +0000

    Added menu endpoint back in after rename

---
+++ server/src/endpoints/menu/Menu.java
@@ -0,0 +1,40 @@
+package endpoints.menu;
+
+import static util.JsonUtil.toJson;
+
+import database.DatabaseManager;
+import database.tables.MenuItem;
+import java.util.List;
+import javax.persistence.EntityManager;
+
+
+public class Menu {
+
+  /**
+   * Gets the full menu from the database and returns it in JSON. No JSON input as it is a get
+   * request.
+   *
+   * @return The menu in JSON as a string.
+   */
+  public static String getMenu() {
+
+    EntityManager entityManager = DatabaseManager.getInstance().getEntityManager();
+
+    List<MenuItem> menuItems = entityManager.createQuery("from MenuItem ", MenuItem.class)
+        .getResultList();
+
+    entityManager.close();
+
+    if (menuItems == null) {
+      return "";
+    }
+
+    MenuData[] menuData = new MenuData[menuItems.size()];
+
+    for (int i = 0; i < menuData.length; i++) {
+      menuData[i] = new MenuData(menuItems.get(i));
+    }
+
+    return toJson(menuData);
+  }
+}
