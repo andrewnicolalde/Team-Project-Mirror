8c19f398876f29ce7e6469b634518107d59d1c5a
Marcus <marcus.messer.2016@live.rhul.ac.uk>
2018-02-21 01:15:49 +0000

    Moved getOrderId to the home page

--- server/src/static/js/customer-menu.js
+++ server/src/static/js/customer-menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  getTransactionId();
   loadMenu();
+  loadOrder();
 });
 
 function loadMenu() {
@@ -36,27 +36,6 @@ function loadMenu() {
   });
 }
 
-function getTransactionId() {
-  get("/api/authTable/getTransactionId", function (data) {
-    var response = JSON.parse(data);
-
-    var transactionId = response.transactionId;
-
-    getOrderId(transactionId);
-  });
-}
-
-function getOrderId(transactionId) {
-  post("/api/authTable/getOrderId", JSON.stringify({
-    transactionId: transactionId
-  }), function (data) {
-    var response = JSON.parse(data);
-
-    localStorage.setItem("orderId", response.orderId);
-    // Load order now, when the orderId has definitely been set.
-    loadOrder();
-  });
-}
 
 function loadOrder() {
   var postData = {orderNumber: localStorage.getItem("orderId")};
