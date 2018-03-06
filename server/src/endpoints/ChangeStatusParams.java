010db58b3e208bd4b962894a5b0d639f548f0004
Marcus Messer <marcus.messer.2016@live.rhul.ac.uk>
2018-03-06 12:44:47 +0000

    Moved changestatus params till the endpoints package

---
+++ server/src/endpoints/ChangeStatusParams.java
@@ -0,0 +1,25 @@
+package endpoints;
+
+/**
+ * This class coverts JSON into usable Java Objects.
+ *
+ * @author Marcus Messer
+ */
+public class ChangeStatusParams {
+
+  private String newOrderStatus;
+  private Long orderId;
+
+  public ChangeStatusParams(Long orderId, String newOrderStatus) {
+    this.newOrderStatus = newOrderStatus;
+    this.orderId = orderId;
+  }
+
+  public String getNewOrderStatus() {
+    return newOrderStatus;
+  }
+
+  public Long getId() {
+    return orderId;
+  }
+}
