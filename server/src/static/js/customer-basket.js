2785d1a0c12f9505921d92e9797a0023f7fb0409
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2018-03-08 17:25:37 +0000

    Add card payment
    Transaction totals are now real.

--- server/src/static/js/customer-basket.js
+++ server/src/static/js/customer-basket.js
@@ -27,3 +27,34 @@ $(document).ready(function () {
         }
       });
 });
+
+/**
+ * This function is a helper function which gives getTransactionTotal a
+ * transactionId.
+ * @returns {Promise} transaction ID
+ */
+function getTransactionId(){
+  return new Promise(function (resolve, reject) {
+    get("/api/authTable/getTransactionId", function(data) {
+      var response = JSON.parse(data);
+      //console.log(response.transactionId);
+      resolve(response.transactionId);
+    });
+  });
+}
+
+/**
+ * This function is responsible for
+ * @param transactionId
+ * @returns the total price of the transaction
+ */
+function getTransactionTotal(transactionId) {
+  var dataToSend = JSON.stringify({
+    transactionId: transactionId
+  });
+  return new Promise(function (resolve, reject){
+    post("/api/authTable/getTransactionTotal", dataToSend, function(data){
+      resolve(data);
+    });
+  });
+}
