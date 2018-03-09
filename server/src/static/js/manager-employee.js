ae8b98fd42ec4bf46bde71a1f2f12dcc52343590
Toby Such <toby@suchfamily.com>
2018-03-09 03:19:23 +0000

    Fixed confirm button not appearing. Added code to return table to normal view mode after editting

--- server/src/static/js/manager-employee.js
+++ server/src/static/js/manager-employee.js
@@ -20,11 +20,19 @@ function loadEmployees() {
 }
 
 function startEdit(id) {
-  $("#emp-first-" + id).html("<input>");
-  $("#emp-last-" + id).html("<input>");
-  $("#emp-department-" + id).html("<input>");
+  $("#emp-first-" + id).html("<input id='emp-first-" + id + "-input' value='" + $("#emp-first-" + id).text() + "'>");
+  $("#emp-last-" + id).html("<input id='emp-last-" + id + "-input' value='" + $("#emp-last-" + id).text() + "'>");
+  $("#emp-department-" + id).html("<input id='emp-department-" + id + "-input' value='" + $("#emp-department-" + id).text() + "'>");
   $("#edit-" + id).remove();
-  $("#actions-" + id).prepend("<i id='confirm-" + id + "' class='fa fa-check fa-lg confirm'");
+  $("#actions-" + id).prepend("<i id='confirm-" + id + "' class='fa fa-check fa-lg confirm' onclick='confirmEdit(" + id + ")'></i>");
+}
+
+function confirmEdit(id) {
+  $("#emp-first-" + id).html($("#emp-first-" + id + "-input").val());
+  $("#emp-last-" + id).html($("#emp-last-" + id + "-input").val());
+  $("#emp-department-" + id).html($("#emp-department-" + id + "-input").val());
+  $("#actions-" + id).prepend("<i id='edit-" + id + "' class=\"fas fa-edit fa-lg edit\" onclick='startEdit(" + id + ");'></i>")
+  $("#confirm-" + id).remove();
 }
 
 function remove(id) {
