7b3bc632333fb0b81775e710abd091c6e0dad229
Toby Such <toby@suchfamily.com>
2018-03-18 22:59:02 +0000

    Started adding javascript which generates the table of items

--- server/src/static/js/manager-menu.js
+++ server/src/static/js/manager-menu.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
   get("/api/authStaff/getMenu", function(data) {
-    console.log(data);
+    menuItems = JSON.parse(data);
+    for (let  i = 0; i < menuItems.length; i++) {
+      const item = menuItems[i];
+      $("#menu").append("<td>" + item.name + "</td>\n"
+          + "<td>" + item.description + "</td>\n"
+          + "<td>" + item.category + "</td>"
+          + "<td>£" + parseFloat(item.price).toFixed(2) + "</td>\n"
+          + "<td>" + item.calories + "kCal</td>\n"
+          + "<td></td>\n"
+          + "<td>" + item.ingredients + "</td>\n"
+          + "<td>" + item.picture_src + "</td>\n"
+          + "<td></td>");
+    }
   });
 });
